fb668a9d744ff373eb96386485d651b600de3712
Gerasko-Vadim <vadim.gerasko99@gmail.com>
2021-02-11 12:59:06 +0600

    fixed

--- src/components/dishesInfo/dishesInfo.jsx
+++ src/components/dishesInfo/dishesInfo.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React,{useEffect} from "react";
 import CardDishes from "../main/dishes/cards/cards";
 import clas from "./dishesInfo.module.css";
 import Ordering from "./ordering/ordering";
 import Slider from "./slider/slider";
 
 const DishesInfo = () => {
+  useEffect(()=>{
+    window.scrollTo(0,0)
+  },[])
   return (
     <>
       <Slider />
